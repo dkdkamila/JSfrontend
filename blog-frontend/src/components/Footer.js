48ca13c5892c92f0190da561b86d79b7e0090c81
dkdkamila <dominika.kamila.berg@gmail.com>
2024-01-12 16:18:49 +0100

    components

---
+++ blog-frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="mainfooter">
+            <p>&copy; 2024 Dominika Berg</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ blog-frontend/src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importera Link från react-router-dom 
+
+function Navigation() {
+    return (
+        <section className="navigation">
+            <h2>Navigering</h2>
+            <nav id="mainmenu">
+                <ul>
+                    <li><Link to="/blogg">Startsidan</Link></li>
+                    <li><Link to="/admin">Adminsida- hantera inlägg</Link></li>
+                </ul>
+            </nav>
+
+        </section>
+    );
+}
+
+export default Navigation;
