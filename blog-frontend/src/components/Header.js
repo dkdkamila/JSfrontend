5e4aff51b64aeeeba93e2348995e2894fbcc0c02
dkdkamila <dominika.kamila.berg@gmail.com>
2024-01-12 16:17:16 +0100

    filstruktur

---
+++ blog-frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+    return (
+        <header className="mainheader">
+            <img src="/Users/dominikaberg/Desktop/JSfrontend/blog-frontend/src/siberian.png" alt="header bild med 2 katter" title="sibiriska katter" className="bild" />
+        </header>
+    );
+}
+
+export default Header;
