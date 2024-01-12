eb2511382c5c5e5ac061cfaf75300fcf3c16551d
dkdkamila <dominika.kamila.berg@gmail.com>
2024-01-12 16:36:27 +0100

    Switch

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
